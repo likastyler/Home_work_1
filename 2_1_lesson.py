fca723b9b8f2db19d590e9c94f04dc8c9d434ae0
likastyler <rustamalymkulov@gmail.com>
2015-11-16 19:02:58 -0800

    Made some changes

---
+++ 2_1_lesson.py
@@ -0,0 +1,10 @@
+>>> print 'Society in XXI Century'
+Society in XXI Century
+>>> age = int(raw_input('Please enter your age'))
+Please enter your age12
+>>> if 0 <= age <7: print 'You are in the kindergarden'
+elif 7 <= age < 18: print 'You are in the school'
+elif 18 <= age < 25: print 'You are in vocational school'
+elif 25 <= age < 60: print 'You are in work'
+elif 60 <= age < 120: print 'You have a choice'
+else: print 'Error! This program is for people!'
